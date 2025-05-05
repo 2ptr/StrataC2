2408199ef087f02a34b58c77f5fcc954ea2b2ad1
jake.otte <jotte@tannerco.com>
2025-05-05 16:13:27 -0400

    asdf

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_StrataC2_FILE C:/Users/jake.otte/Desktop/projects/StrataC2/cmake-build-debug/StrataC2.exe)
+set(__QT_DEPLOY_TARGET_StrataC2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_StrataC2_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
